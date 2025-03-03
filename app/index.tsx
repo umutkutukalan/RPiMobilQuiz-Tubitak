2d935f817779765b4afb6fed4a26e66ae6388256
kutukalan <118997325+umutkutukalan@users.noreply.github.com>
2025-03-04 02:45:44 +0300

    feat: add react-native-vector-icons dependency

--- app/index.tsx
+++ app/index.tsx
@@ -2,7 +2,7 @@ import { Text, View } from "react-native";
 
 export default function Index() {
   return (
-    <View className="h-full flex items-center justify-center ">
+    <View className="h-full flex items-center justify-center">
       <Text className="text-lg font-bold">Beyza Nur Bostancıoğlu</Text>
       <Text className="text-lg font-bold">Selva Korkmaz</Text>
     </View>
