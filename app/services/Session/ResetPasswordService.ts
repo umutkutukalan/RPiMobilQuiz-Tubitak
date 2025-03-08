b53cc370deba446eeb19ea12d15bcdc1240018bc
kutukalan <118997325+umutkutukalan@users.noreply.github.com>
2025-03-08 04:20:46 +0300

    servisler asenkron (async) yapıdan promise chaning yapısına geçirildi

--- app/services/Session/ResetPasswordService.ts
+++ app/services/Session/ResetPasswordService.ts
@@ -1,29 +1,28 @@
 import axios from "axios";
 import config from "../config";
 
-const resetPassword = async (email: string) => {
-  try {
-    const response = await axios.post(
-      `${config.baseUrl}/Session/reset_password`,
-      {
-        email,
+const resetPassword = (email: string) => {
+  return axios
+    .post(`${config.baseUrl}/Session/reset_password`, { email })
+    .then((response) => {
+      return response;
+    })
+    .catch((error) => {
+      if (axios.isAxiosError(error)) {
+        console.error(
+          "Reset password error:",
+          error.response ? error.response.data : error.message
+        );
+        return Promise.reject(
+          new Error(
+            error.response ? error.response.data : "Reset password failed"
+          )
+        );
+      } else {
+        console.error("Unexpected error:", error);
+        return Promise.reject(new Error("An unexpected error occurred"));
       }
-    );
-    return response;
-  } catch (error) {
-    if (axios.isAxiosError(error)) {
-      console.error(
-        "Reset password error:",
-        error.response ? error.response.data : error.message
-      );
-      throw new Error(
-        error.response ? error.response.data : "Reset password failed"
-      );
-    } else {
-      console.error("Unexpected error:", error);
-      throw new Error("An unexpected error occurred");
-    }
-  }
+    });
 };
 
 export default resetPassword;
