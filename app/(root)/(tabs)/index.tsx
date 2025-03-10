bcbd22c14ebf671b8682eac64d366207eadae449
kutukalan <118997325+umutkutukalan@users.noreply.github.com>
2025-03-11 02:16:37 +0300

    feat: added soru-olustur and quiz-olustur pages

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -6,11 +6,11 @@ export default function Index() {
     <View>
       <Link href="/sign-in">SıgnIn</Link>
       <Link href="/profile">Profile</Link>
-      <Link href="/hesap_bilgileri">Hesap Bilgileri</Link> 
+      <Link href="/hesap_bilgileri">Hesap Bilgileri</Link>
       <Link href="/password_update">Şifre Güncelle</Link>
       <Link href="/edit_profile">Profili Düzenle</Link>
       <Link href="/quiz-olustur">Quiz Oluştur</Link>
+      <Link href="/soru-olustur">Soru Oluştur</Link>
     </View>
   );
 }
-
---
+++ app/(root)/(tabs)/quiz-olustur.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const QuizOlustur = () => {
+  return (
+    <View>
+      <Text>quiz-olustur</Text>
+    </View>
+  );
+};
+
+export default QuizOlustur;
