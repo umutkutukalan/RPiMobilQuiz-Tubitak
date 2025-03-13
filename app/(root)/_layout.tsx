b678641a4b2449bcfee938fc4a54b8f2e39bfa4d
kutukalan <118997325+umutkutukalan@users.noreply.github.com>
2025-03-14 00:17:50 +0300

    feat: güncellenmiş yönlendirme ile Slot bileşeni eklendi

--- app/(root)/_layout.tsx
+++ app/(root)/_layout.tsx
@@ -1,9 +1,10 @@
 import { View, Text } from "react-native";
 import React from "react";
-import { Redirect } from "expo-router";
+import { Redirect, Slot } from "expo-router";
 
 const AppLayout = () => {
-  return <Redirect href="/sign-in" />;
+  // return <Redirect href="/sign-in" />;
+  return <Slot />;
 };
 
 export default AppLayout;
