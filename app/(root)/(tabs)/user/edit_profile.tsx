1df9b3cf949c3c41236b3fcd0c0e9e84e42648fe
kutukalan <118997325+umutkutukalan@users.noreply.github.com>
2025-03-13 17:51:48 +0300

    style: edit_profile sayfasındaki stil güncellemeleri yapıldı, düzen ve görünüm iyileştirildi

--- app/(root)/(tabs)/user/edit_profile.tsx
+++ app/(root)/(tabs)/user/edit_profile.tsx
@@ -14,51 +14,56 @@ const EditProfileForm = () => {
   const [phone, setPhone] = useState("");
 
   return (
-    <ScrollView className="bg-[#F3EDF7]">
-      <View className="bg-white border border-[#DDA0DD] p-5 rounded-lg">
-        <Text
-        className="text-[#800080] mb-5 text-2xl font-semibold"
-        >
-          Hesap Bilgilerini Güncelle
-        </Text>
+    <ScrollView>
+      <View className="h-full flex flex-col gap-10">
+        <View className="flex flex-col gap-2">
+          <View className="flex flex-col gap-1">
+            <Text className="text-[#800080]">Ad</Text>
+            <TextInput
+              placeholder="Adınızı giriniz"
+              value={firstName}
+              onChangeText={setFirstName}
+              className="border-2 border-[#E6B0D4] py-5 px-2 text-top rounded-md mb-5"
+            />
+          </View>
 
-        <Text className="text-[#800080] mb-5">Ad</Text>
-        <TextInput
-          placeholder="Adınızı giriniz"
-          value={firstName}
-          onChangeText={setFirstName}
-          className="border-2 border-[#E6B0D4] p-2 rounded-md mb-5"
-        />
+          <View className="flex flex-col gap-1">
+            <Text className="text-[#800080]">Soyad</Text>
+            <TextInput
+              placeholder="Soyadınızı giriniz"
+              value={surname}
+              onChangeText={setSurname}
+              className="border-2 border-[#E6B0D4] py-5 px-2 text-top rounded-md mb-5"
+            />
+          </View>
 
-        <Text className="text-[#800080] mb-5">Soyad</Text>
-        <TextInput
-          placeholder="Soyadınızı giriniz"
-          value={surname}
-          onChangeText={setSurname}
-          className="border-2 border-[#E6B0D4] p-2 rounded-md mb-5"
-        />
+          <View className="flex flex-col gap-1">
+            <Text className="text-[#800080]">E-posta</Text>
+            <TextInput
+              placeholder="E-posta adresinizi giriniz"
+              value={email}
+              onChangeText={setEmail}
+              keyboardType="email-address"
+              className="border-2 border-[#E6B0D4] py-5 px-2 text-top rounded-md mb-5"
+            />
+          </View>
 
-        <Text className="text-[#800080] mb-5">E-posta</Text>
-        <TextInput
-          placeholder="E-posta adresinizi giriniz"
-          value={email}
-          onChangeText={setEmail}
-          keyboardType="email-address"
-          className="border-2 border-[#E6B0D4] p-2 rounded-md mb-5"
-        />
-
-        <Text className="text-[#800080] mb-5">Telefon</Text>
-        <TextInput
-          placeholder="Telefon numaranızı giriniz"
-          value={phone}
-          onChangeText={setPhone}
-          keyboardType="phone-pad"
-          className="border-2 border-[#E6B0D4] p-2 rounded-md mb-5"
-        />
-
-        <TouchableOpacity className="bg-[#1C1C64] p-2 rounded-md">
-          <Text className="text-white text-center">Güncelle</Text>
-        </TouchableOpacity>
+          <View className="flex flex-col gap-1">
+            <Text className="text-[#800080]">Telefon</Text>
+            <TextInput
+              placeholder="Telefon numaranızı giriniz"
+              value={phone}
+              onChangeText={setPhone}
+              keyboardType="phone-pad"
+              className="border-2 border-[#E6B0D4] py-5 px-2 text-top rounded-md mb-5"
+            />
+          </View>
+        </View>
+        <View>
+          <TouchableOpacity className="bg-[#1C1C64] py-5 px-2 text-top rounded-md">
+            <Text className="text-white text-center text-2xl">Güncelle</Text>
+          </TouchableOpacity>
+        </View>
       </View>
     </ScrollView>
   );
