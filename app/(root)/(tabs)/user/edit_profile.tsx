3b1d9104b72eaf49c4a0747dabc1cb12c8ae1839
kutukalan <118997325+umutkutukalan@users.noreply.github.com>
2025-03-13 17:37:44 +0300

    style: updated edit_profile page styles

--- app/(root)/(tabs)/user/edit_profile.tsx
+++ app/(root)/(tabs)/user/edit_profile.tsx
@@ -1,57 +1,67 @@
-import React, { useState } from 'react';
-import { View, Text, TextInput, TouchableOpacity, ScrollView } from 'react-native';
+import React, { useState } from "react";
+import {
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  ScrollView,
+} from "react-native";
 
 const EditProfileForm = () => {
-    const [firstName, setFirstName] = useState('');
-    const [surname, setSurname] = useState('');
-    const [email, setEmail] = useState('');
-    const [phone, setPhone] = useState('');
+  const [firstName, setFirstName] = useState("");
+  const [surname, setSurname] = useState("");
+  const [email, setEmail] = useState("");
+  const [phone, setPhone] = useState("");
 
-    return (
-        <ScrollView style={{ padding: 20, backgroundColor: '#F3EDF7' }}>
-            <View style={{ backgroundColor: 'white', padding: 20, borderRadius: 10, borderColor: '#DDA0DD', borderWidth: 2 }}>
-                <Text style={{ fontSize: 18, fontWeight: 'bold', color: '#800080', marginBottom: 10 }}>Hesap Bilgilerini Güncelle</Text>
-                
-                <Text style={{ color: '#800080', marginBottom: 5 }}>Ad</Text>
-                <TextInput
-                    placeholder="Adınızı giriniz"
-                    value={firstName}
-                    onChangeText={setFirstName}
-                    style={{ borderWidth: 2, borderColor: '#E6B0D4', padding: 10, borderRadius: 5, marginBottom: 10 }}
-                />
-                
-                <Text style={{ color: '#800080', marginBottom: 5 }}>Soyad</Text>
-                <TextInput
-                    placeholder="Soyadınızı giriniz"
-                    value={surname}
-                    onChangeText={setSurname}
-                    style={{ borderWidth: 2, borderColor: '#E6B0D4', padding: 10, borderRadius: 5, marginBottom: 10 }}
-                />
-                
-                <Text style={{ color: '#800080', marginBottom: 5 }}>E-posta</Text>
-                <TextInput
-                    placeholder="E-posta adresinizi giriniz"
-                    value={email}
-                    onChangeText={setEmail}
-                    keyboardType="email-address"
-                    style={{ borderWidth: 2, borderColor: '#E6B0D4', padding: 10, borderRadius: 5, marginBottom: 10 }}
-                />
-                
-                <Text style={{ color: '#800080', marginBottom: 5 }}>Telefon</Text>
-                <TextInput
-                    placeholder="Telefon numaranızı giriniz"
-                    value={phone}
-                    onChangeText={setPhone}
-                    keyboardType="phone-pad"
-                    style={{ borderWidth: 2, borderColor: '#E6B0D4', padding: 10, borderRadius: 5, marginBottom: 20 }}
-                />
-                
-                <TouchableOpacity style={{ backgroundColor: '#1C1C64', padding: 10, borderRadius: 5 }}>
-                    <Text style={{ color: 'white', textAlign: 'center' }}>Güncelle</Text>
-                </TouchableOpacity>
-            </View>
-        </ScrollView>
-    );
+  return (
+    <ScrollView className="bg-[#F3EDF7]">
+      <View className="bg-white border border-[#DDA0DD] p-5 rounded-lg">
+        <Text
+        className="text-[#800080] mb-5 text-2xl font-semibold"
+        >
+          Hesap Bilgilerini Güncelle
+        </Text>
+
+        <Text className="text-[#800080] mb-5">Ad</Text>
+        <TextInput
+          placeholder="Adınızı giriniz"
+          value={firstName}
+          onChangeText={setFirstName}
+          className="border-2 border-[#E6B0D4] p-2 rounded-md mb-5"
+        />
+
+        <Text className="text-[#800080] mb-5">Soyad</Text>
+        <TextInput
+          placeholder="Soyadınızı giriniz"
+          value={surname}
+          onChangeText={setSurname}
+          className="border-2 border-[#E6B0D4] p-2 rounded-md mb-5"
+        />
+
+        <Text className="text-[#800080] mb-5">E-posta</Text>
+        <TextInput
+          placeholder="E-posta adresinizi giriniz"
+          value={email}
+          onChangeText={setEmail}
+          keyboardType="email-address"
+          className="border-2 border-[#E6B0D4] p-2 rounded-md mb-5"
+        />
+
+        <Text className="text-[#800080] mb-5">Telefon</Text>
+        <TextInput
+          placeholder="Telefon numaranızı giriniz"
+          value={phone}
+          onChangeText={setPhone}
+          keyboardType="phone-pad"
+          className="border-2 border-[#E6B0D4] p-2 rounded-md mb-5"
+        />
+
+        <TouchableOpacity className="bg-[#1C1C64] p-2 rounded-md">
+          <Text className="text-white text-center">Güncelle</Text>
+        </TouchableOpacity>
+      </View>
+    </ScrollView>
+  );
 };
 
 export default EditProfileForm;
