667e860dec90b702962e5f17be91e7ef44b43b6b
kutukalan <118997325+umutkutukalan@users.noreply.github.com>
2025-03-16 16:40:11 +0300

    feat: platforma göre veri saklama yöntemleri güncellendi, web için localStorage desteği eklendi

--- config/strogeService.ts
+++ config/strogeService.ts
@@ -1,16 +1,26 @@
+import { Platform } from "react-native";
 import * as SecureStore from "expo-secure-store";
 
 export const strogeService = {
-  store: async (key: string, value: any) => {
-    await SecureStore.setItemAsync(key, JSON.stringify(value)); // JSON formatına çevir
+  store: async (key: string, value: string) => {
+    if (Platform.OS === "web") {
+      localStorage.setItem(key, value); // Web için localStorage kullan
+    } else {
+      await SecureStore.setItemAsync(key, value); // Mobilde SecureStore kullan
+    }
   },
-
   get: async (key: string) => {
-    const value = await SecureStore.getItemAsync(key);
-    return value ? JSON.parse(value) : null; // JSON'dan nesneye çevir
+    if (Platform.OS === "web") {
+      return localStorage.getItem(key); // Web için localStorage kullan
+    } else {
+      return await SecureStore.getItemAsync(key); // Mobilde SecureStore kullan
+    }
   },
-
   remove: async (key: string) => {
-    await SecureStore.deleteItemAsync(key);
+    if (Platform.OS === "web") {
+      localStorage.removeItem(key); // Web için localStorage kullan
+    } else {
+      await SecureStore.deleteItemAsync(key); // Mobilde SecureStore kullan
+    }
   },
 };
